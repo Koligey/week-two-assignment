b21ef0c67342ed9b79c96b25fdf9a2a03dccba10
ch3nacity <44324874+Koligey@users.noreply.github.com>
2019-05-06 17:57:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function = myFunction (){
-  var side1 = parseInt(document.getElementById("side1").value);
-  var side2 = parseInt(document.getElementById("side2").value);
-  var side3 = parseInt(document.getElementById("side3").value);
-  if(side1 <=0 || side2<=0 || side3<=0 {
+ function myFunction() {
+  var side1 = parseInt(document.getElementById("length1").value);
+  var side2 = parseInt(document.getElementById("length2").value);
+  var side3 = parseInt(document.getElementById("length3").value);
+  if(side1 <=0 || side2<=0 || side3<=0 ){
     alert("INVALID OPERATION");
   }
-
-  elseif (side1===side2 || side2===side3 || side1===side3){
+  else if(side1 + side2 <= side3 || side2 + side3 <=side1 || side1 + side3 <= side2) {
+    alert ("NOT A TRIANGLE")
+  }
+  else if (side1===side2 && side2===side3 && side1===side3){
     alert ("EQUILATERAL TRIANGLE");
   }
-  elseif(side1 ===side2 && side1!==side3) ||
-  (side2===side3 && side2 !==) ||
-  (side3===side1 && side1 !==side2)) {
+  else if(side1 === side2 || side1===side3 || side2===side3 ) {
     alert ("ISOSCELES TRIANGLE");
   }
-  elseif(side1!==side2 || side2!==side3 || side3!==side1){
+  else if(side1!==side2 && side2!==side3){
     alert("SCALENE TRIANGLE")
   }
-  elseif(side1 + side2 <= side3 || side2 + side3 <=side1) ||
-  (side1 + side3 <= side2) {
-    alert ("NOT A TRIANGLE");
-}
+
+
+};
